7ab9021470178e3f580b96aa9ef08752a5d34d93
Neil <santoscneil@gmail.com>
2023-01-31 23:35:29 +0800

    deleted node modules

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import Typed from 'react-typed';
+import kae from '../assets/Kae.jpg';
+import ob from '../assets/OB.jpg';
+import jha from '../assets/Jhasset.png';
+import niel from '../assets/NeilC.jpg';
+import elai from '../assets/Elaizaa.jpg';
+import wsm from '../assets/WSM.jpg';
 
 function AboutUs() {
   return (
@@ -27,21 +33,21 @@ function AboutUs() {
           <div className="flex justify-center">
             <div className="w-28 h-28 bg-white rounded-full mr-3">
               <img
-                src="src\assets\OB.jpg"
+                src={ob}
                 alt="Oniel Balicoco"
                 className="rounded-full w-28 h-28"
               />
             </div>
             <div className="w-28 h-28 bg-white rounded-full mr-3">
               <img
-                src="src\assets\Kae.jpg"
+                src={kae}
                 alt="Kae Lacap"
                 className="rounded-full w-28 h-28"
               />
             </div>
             <div className="w-28 h-28 bg-white rounded-full mr-3">
               <img
-                src="src\assets\Jhasset.png"
+                src={jha}
                 alt="Jhasset Tipalan"
                 className="rounded-full object-cover w-full h-full"
               />
@@ -50,14 +56,14 @@ function AboutUs() {
           <div className="flex justify-center">
             <div className="w-28 h-28 bg-white rounded-full mr-3">
               <img
-                src="src\assets\NeilC.jpg"
+                src={niel}
                 alt="Neil Santos"
                 className="rounded-full object-cover w-full h-full"
               />
             </div>
             <div className="w-28 h-28 bg-white rounded-full mr-3">
               <img
-                src="src\assets\Elaizaa.jpg"
+                src={elai}
                 alt="Elaiza Ilagan"
                 className="rounded-full object-cover w-full h-full"
               />
@@ -66,7 +72,7 @@ function AboutUs() {
         </div>
         <div className="aspect-ratio 1/1 bg-slate-400 w-1/2 absolute top-56 h-1/2 right-28">
           <img
-            src="src\assets\WSM.jpg"
+            src={wsm}
             alt="With Sir Mark"
             className="object-cover w-full h-full"
           />
