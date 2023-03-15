49db10a89252f2e4b64503f6fa0831961a93fed5
Paul Leandro Lanot <paul.leanot.aesiph@gmail.com>
2023-03-15 23:14:21 +0800

    4.0.0

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -10,7 +10,7 @@ import wsm from '../assets/WSM.jpg';
 
 function AboutUs() {
   return (
-    <Element className="p-20" id="about-us">
+    <Element className="p-20 max-lg:p-0 " id="about-us">
       <div className="flex place-content-end">
         <span className="text-7xl ">
           <Typed
@@ -22,8 +22,8 @@ function AboutUs() {
           />
         </span>
       </div>
-      <div className="relative h-screen flex flex-col md:flex-row">
-        <div className="bg-slate-800 w-full md:w-1/2 lg:w-1/2 xl:w-1/2 absolute h-1/2 md:h-auto md:h-1/2 top-0 md:top-14 left-0 md:left-14 text-white text-justify p-6 md:shrink-0">
+      <div className="relative h-screen flex flex-col md:flex-row ">
+        <div className="bg-slate-800 w-full md:w-1/2 lg:w-1/2 xl:w-1/2 absolute h-1/2 max-md:relative md:h-1/2 top-0 md:top-14 left-0 md:left-14 text-white text-justify p-6 md:shrink-0">
           <p className="text-4xl md:text-5xl lg:text-6xl">OUR TEAM</p>
           <div className="mt-4 mb-4">
             <p className="text-base md:text-lg lg:text-xl">
@@ -31,43 +31,43 @@ function AboutUs() {
               Engr.Mark Melegrito
             </p>
           </div>
-          <div className="flex justify-center">
-            <div className="w-28 h-28 bg-white rounded-full mr-3">
-              <img
-                src={ob}
-                alt="Oniel Balicoco"
-                className="rounded-full max-w-full h-full mx-auto"
-              />
-            </div>
-            <div className="w-28 h-28 bg-white rounded-full mr-3 md:block">
-              <img
-                src={kae}
-                alt="Kae Lacap"
-                className="rounded-full max-w-full h-full mx-auto"
-              />
-            </div>
-            <div className="w-28 h-28 bg-white rounded-full mr-3 md:block">
-              <img
-                src={jha}
-                alt="Jhasset Tipalan"
-                className="rounded-full max-w-full h-full mx-auto"
-              />
-            </div>
-          </div>
-          <div className="flex justify-center md:flex">
-            <div className="w-28 h-28 bg-white rounded-full mr-3 md:block">
-              <img
-                src={niel}
-                alt="Neil Santos"
-                className="rounded-full max-w-full h-full mx-auto"
-              />
-            </div>
-            <div className="w-28 h-28 bg-white rounded-full mr-3 md:block">
-              <img
-                src={elai}
-                alt="Elaiza Ilagan"
-                className="rounded-full max-w-full h-full mx-auto"
-              />
+          <div className="flex items-center justify-center h-max ">
+            <div className="parent">
+              <div className="w-28 h-28 max-md:w-16 max-md:h-16 bg-white rounded-full mr-3 div1">
+                <img
+                  src={ob}
+                  alt="Oniel Balicoco"
+                  className="rounded-full max-w-full h-full mx-auto"
+                />
+              </div>
+              <div className="w-28 h-28 max-md:w-16 max-md:h-16 bg-white rounded-full mr-3 md:block div2">
+                <img
+                  src={kae}
+                  alt="Kae Lacap"
+                  className="rounded-full max-w-full h-full mx-auto"
+                />
+              </div>
+              <div className="w-28 h-28 max-md:w-16 max-md:h-16 bg-white rounded-full mr-3 md:block div3">
+                <img
+                  src={jha}
+                  alt="Jhasset Tipalan"
+                  className="rounded-full max-w-full h-full mx-auto"
+                />
+              </div>
+              <div className="w-28 h-28 max-md:w-16 max-md:h-16 bg-white rounded-full mr-3 md:block div4">
+                <img
+                  src={niel}
+                  alt="Neil Santos"
+                  className="rounded-full max-w-full h-full mx-auto"
+                />
+              </div>
+              <div className="w-28 h-28 max-md:w-16 max-md:h-16 bg-white rounded-full mr-3 div5">
+                <img
+                  src={elai}
+                  alt="Elaiza Ilagan"
+                  className="rounded-full max-w-full h-full mx-auto"
+                />
+              </div>
             </div>
           </div>
         </div>
